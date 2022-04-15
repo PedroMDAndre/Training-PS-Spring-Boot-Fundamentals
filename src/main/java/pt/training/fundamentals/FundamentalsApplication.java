41055812855e13702ba0ce06826b98d6d5278009
PedroMDAndre <pedro.mda@gmail.com>
2022-04-15 15:07:20 +0100

    1st commit

---
+++ src/main/java/pt/training/fundamentals/FundamentalsApplication.java
@@ -0,0 +1,13 @@
+package pt.training.fundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/training/fundamentals/FundamentalsApplicationTests.java
@@ -0,0 +1,15 @@
+package pt.training.fundamentals;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+
+}
